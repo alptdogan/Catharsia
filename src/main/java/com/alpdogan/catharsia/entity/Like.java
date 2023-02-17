39b8aa017122abc1a5829948f8fea4b95cb13aab
Alptuğ Doğan <alpdogan000@gmail.com>
2023-02-17 12:31:26 +0300

    Create Like entity

---
+++ src/main/java/com/alpdogan/catharsia/entity/Like.java
@@ -0,0 +1,31 @@
+package com.alpdogan.catharsia.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "like")
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column (name = "id")
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE,
+            CascadeType.DETACH,
+            CascadeType.REFRESH
+    })
+    private Comment comment;
+
+
+}
