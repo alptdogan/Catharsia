464622f79d28e98d4df21c7f3d0cd1677360e118
Alptuğ Doğan <alpdogan000@gmail.com>
2023-02-17 13:51:35 +0300

    Create TopicRepository

---
+++ src/main/java/com/alpdogan/catharsia/service/CommentService.java
@@ -0,0 +1,40 @@
+package com.alpdogan.catharsia.service;
+
+import com.alpdogan.catharsia.entity.Comment;
+import com.alpdogan.catharsia.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+@Transactional
+public class CommentService {
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    public List<Comment> getAllComments() {
+        return commentRepository.findAll();
+    }
+
+    public Comment getCommentById(int id) {
+        return commentRepository.findCommentById(id);
+    }
+
+    public void createComment(Comment comment) {
+        commentRepository.save(comment);
+    }
+
+    public void updateCommentById(int id, Comment comment) {
+        comment.setId(id);
+        commentRepository.save(comment);
+    }
+
+    public void deleteCommentById(int id) {
+        commentRepository.delete(commentRepository.findCommentById(id));
+    }
+
+}
