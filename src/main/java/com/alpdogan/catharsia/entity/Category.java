363d00f08f4603f2a4aff69e3fcf37b6e6985fab
Alptuğ Doğan <alpdogan000@gmail.com>
2023-02-17 14:09:01 +0300

    Create Category entity

---
+++ src/main/java/com/alpdogan/catharsia/entity/Category.java
@@ -0,0 +1,37 @@
+package com.alpdogan.catharsia.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column (name = "id")
+    private int id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE,
+            CascadeType.DETACH,
+            CascadeType.REFRESH
+    })
+    private List<Topic> topics = new ArrayList<>();
+
+
+}
