5ec25abd8bba62cbe657bbb21d9807b770539a2a
Alptuğ Doğan <alpdogan000@gmail.com>
2023-02-17 12:25:54 +0300

    Initial commit

---
+++ src/main/java/com/alpdogan/catharsia/CatharsiaApplication.java
@@ -0,0 +1,13 @@
+package com.alpdogan.catharsia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatharsiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatharsiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alpdogan/catharsia/CatharsiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpdogan.catharsia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatharsiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
