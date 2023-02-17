35d04fc330eb08e4d661b87b995d7c60fdaadc42
Alptuğ Doğan <alpdogan000@gmail.com>
2023-02-17 13:55:40 +0300

    Create UserService

---
+++ src/main/java/com/alpdogan/catharsia/service/UserService.java
@@ -0,0 +1,39 @@
+package com.alpdogan.catharsia.service;
+
+import com.alpdogan.catharsia.entity.User;
+import com.alpdogan.catharsia.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(int id) {
+        return userRepository.findUserById(id);
+    }
+
+    public void createUser(User user) {
+        userRepository.save(user);
+    }
+
+    public void updateUserById(int id, User user) {
+        user.setId(id);
+        userRepository.save(user);
+    }
+
+    public void deleteUserById(int id) {
+        userRepository.delete(userRepository.findUserById(id));
+    }
+
+}
