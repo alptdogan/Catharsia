479875677ef5393c0cc0d72d288703246b0ab81d
Alptuğ Doğan <alpdogan000@gmail.com>
2023-02-17 12:38:53 +0300

    Create LikeRepository

---
+++ src/main/java/com/alpdogan/catharsia/repository/LikeRepository.java
@@ -0,0 +1,9 @@
+package com.alpdogan.catharsia.repository;
+
+
+import com.alpdogan.catharsia.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, Integer> {
+
+}
