4c2046c9ae11a7de6bdd8b6181f420caf2504023
Alptuğ Doğan <alpdogan000@gmail.com>
2023-02-19 12:25:30 +0300

    add "/topics" endpoint

--- src/main/java/com/alpdogan/catharsia/controller/TopicController.java
+++ src/main/java/com/alpdogan/catharsia/controller/TopicController.java
@@ -1,10 +1,15 @@
 package com.alpdogan.catharsia.controller;
 
+import com.alpdogan.catharsia.entity.Topic;
 import com.alpdogan.catharsia.service.CommentService;
 import com.alpdogan.catharsia.service.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/topics")
@@ -16,6 +21,14 @@ public class TopicController {
     @Autowired
     CommentService commentService;
 
+    @GetMapping
+    public String displayClients (Model model) {
 
+        List<Topic> topics = topicService.getAllTopics();
+        model.addAttribute("topics", topics);
+
+        return "list-topics";
+
+    }
 
 }
