8c7dfbb6521314f87ca51fc0d6410bb35002369a
Alptuğ Doğan <alpdogan000@gmail.com>
2023-02-19 13:09:50 +0300

    add "/updateLike" endpoint

--- src/main/java/com/alpdogan/catharsia/controller/LikeController.java
+++ src/main/java/com/alpdogan/catharsia/controller/LikeController.java
@@ -5,10 +5,7 @@ import com.alpdogan.catharsia.service.LikeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/likes")
@@ -35,6 +32,17 @@ public class LikeController {
         likeService.createLike(like);
 
         return "redirect:/likes";
+
+    }
+
+    // how to update likes???
+    @GetMapping("/updateLike")
+    public String displayLikeUpdateForm(@RequestParam("id") int id, Model model) {
+
+        Like like = likeService.getAllLikes().get(id)//.setId(?);
+        model.addAttribute("like", like);
+
+        return "new-like";
 
     }
 
