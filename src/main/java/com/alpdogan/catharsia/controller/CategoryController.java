f9f4523e8020f2e9666d83a5bdaf30372a893512
Alptuğ Doğan <alpdogan000@gmail.com>
2023-02-19 12:55:23 +0300

    add "/categories" endpoint

--- src/main/java/com/alpdogan/catharsia/controller/CategoryController.java
+++ src/main/java/com/alpdogan/catharsia/controller/CategoryController.java
@@ -1,10 +1,15 @@
 package com.alpdogan.catharsia.controller;
 
+import com.alpdogan.catharsia.entity.Category;
 import com.alpdogan.catharsia.service.CategoryService;
 import com.alpdogan.catharsia.service.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/categories")
@@ -15,6 +20,16 @@ public class CategoryController {
 
     @Autowired
     TopicService topicService;
+
+    @GetMapping
+    public String displayCategories (Model model) {
+
+        List<Category> categories = categoryService.getAllCategories();
+        model.addAttribute("category", categories);
+
+        return "list-categories";
+
+    }
 
 
 
