f8fcc5adf6cdf6c119dfe1270744e17bdbfe6e04
Alptuğ Doğan <alpdogan000@gmail.com>
2023-02-19 12:09:36 +0300

    Update UserController, add "/addUser" endpoint

--- src/main/java/com/alpdogan/catharsia/controller/UserController.java
+++ src/main/java/com/alpdogan/catharsia/controller/UserController.java
@@ -9,13 +9,12 @@ import com.alpdogan.catharsia.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
@@ -52,7 +51,16 @@ public class UserController {
 
     }
 
+    @PostMapping("/addUSer")
+    public String createUser (@ModelAttribute("user") User user,
+                                   @RequestParam List<Long> topics,
+                                   @RequestParam List<Long> comments) {
 
+        userService.createUser(user);
+
+        return "redirect:/users";
+
+    }
 
 }
 
