705e26c19b02ae634e44785ae179efad5cb2126e
Alptuğ Doğan <alpdogan000@gmail.com>
2023-02-19 12:44:51 +0300

    add "/comments" endpoint

--- src/main/java/com/alpdogan/catharsia/controller/CommentController.java
+++ src/main/java/com/alpdogan/catharsia/controller/CommentController.java
@@ -1,10 +1,15 @@
 package com.alpdogan.catharsia.controller;
 
+import com.alpdogan.catharsia.entity.Comment;
 import com.alpdogan.catharsia.service.CommentService;
 import com.alpdogan.catharsia.service.LikeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/comments")
@@ -16,6 +21,14 @@ public class CommentController {
     @Autowired
     LikeService likeService;
 
+    @GetMapping
+    public String displayComments (Model model) {
 
+        List<Comment> comments = commentService.getAllComments();
+        model.addAttribute("comment", comments);
+
+        return "list-comments";
+
+    }
 
 }
