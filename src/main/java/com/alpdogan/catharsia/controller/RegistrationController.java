87d5cfed43228f6ed5f460ecfb6266b8a1b113cf
Alptuğ Doğan <alpdogan000@gmail.com>
2023-02-21 13:02:42 +0300

    Create RegistrationController

---
+++ src/main/java/com/alpdogan/catharsia/controller/RegistrationController.java
@@ -0,0 +1,44 @@
+package com.alpdogan.catharsia.controller;
+
+import com.alpdogan.catharsia.entity.User;
+import com.alpdogan.catharsia.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/register")
+public class RegistrationController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+
+        model.addAttribute("user", new User());
+
+        return "signup_form";
+
+    }
+
+    @PostMapping("/process_register")
+    public String processRegister(User user) {
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        userRepository.save(user);
+
+        return "register_success";
+
+    }
+
+
+
+}
