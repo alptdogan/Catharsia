598aa3d3cc03ee5e5bb719f290566497593a60d8
Alptuğ Doğan <alpdogan000@gmail.com>
2023-02-21 13:19:58 +0300

    Update DashboardController.java

--- src/main/java/com/alpdogan/catharsia/controller/DashboardController.java
+++ src/main/java/com/alpdogan/catharsia/controller/DashboardController.java
@@ -9,11 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DashboardController {
 
     @RequestMapping("/dashboard")
-    public String displayDashboard(Model model, Authentication authentication)
-    {
+    public String displayDashboard(Model model, Authentication authentication) {
+
         model.addAttribute("username", authentication.getName());
         model.addAttribute("roles", authentication.getAuthorities().toString());
+
         return "dashboard";
+
     }
 
 }
