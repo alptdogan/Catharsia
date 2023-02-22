a8fc1f93edb57e532c7b44921583a410c49bd9cc
Alptuğ Doğan <alpdogan000@gmail.com>
2023-02-22 12:38:51 +0300

    add Role field and its constructor. Also add length to userName and password fields.

--- src/main/java/com/alpdogan/catharsia/entity/User.java
+++ src/main/java/com/alpdogan/catharsia/entity/User.java
@@ -1,20 +1,28 @@
 package com.alpdogan.catharsia.entity;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 @Entity
-@Table(name = "users")
+@Table(	name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "user_name"),
+                @UniqueConstraint(columnNames = "email")
+        })
 public class User {
 
     @Id
@@ -26,10 +34,24 @@ public class User {
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
+
+    @NotNull
     @Column(name = "email", unique = true)
     private String email;
-    @Column(name = "password")
+
+    @NotNull
+    @Column(name = "user_name", unique = true, length = 30)
+    private String username;
+
+    @NotNull
+    @Column(name = "password", length = 120)
     private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(	name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     @OneToMany(fetch = FetchType.LAZY, cascade = {
             CascadeType.PERSIST,
@@ -46,6 +68,12 @@ public class User {
             CascadeType.REFRESH
     })
     private List<Comment> comments = new ArrayList<>();
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 
 
 }
