5dd5ccd4aa4ec0a9c56972e68c766c6d6346d426
Alptuğ Doğan <alpdogan000@gmail.com>
2023-02-22 12:42:04 +0300

    Add new methods to use while registering and logging in

--- src/main/java/com/alpdogan/catharsia/repository/UserRepository.java
+++ src/main/java/com/alpdogan/catharsia/repository/UserRepository.java
@@ -4,9 +4,17 @@ import com.alpdogan.catharsia.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findUserById(int id);
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
 
     @Query("SELECT u FROM User u WHERE u.email = ?1")
     public User findByEmail(String email);
