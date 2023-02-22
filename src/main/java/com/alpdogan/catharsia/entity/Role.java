ee7f71021a76a70dc94f06c829e374d446c15d11
Alptuğ Doğan <alpdogan000@gmail.com>
2023-02-22 12:39:24 +0300

    Create Role entity

---
+++ src/main/java/com/alpdogan/catharsia/entity/Role.java
@@ -0,0 +1,27 @@
+package com.alpdogan.catharsia.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+
+}
