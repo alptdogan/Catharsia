7d4caa7c4de68695aceddcf491199491269c3017
Alptuğ Doğan <alpdogan000@gmail.com>
2023-02-23 14:28:59 +0300

    Update

--- src/main/java/com/alpdogan/catharsia/entity/Role.java
+++ src/main/java/com/alpdogan/catharsia/entity/Role.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
+//@NoArgsConstructor
 @ToString
 @Entity
 @Table(name = "roles")
@@ -23,5 +23,28 @@ public class Role {
     @Column(length = 20)
     private ERole name;
 
+    public Role() {
+
+    }
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public ERole getName() {
+        return name;
+    }
+
+    public void setName(ERole name) {
+        this.name = name;
+    }
 
 }
--- src/main/java/com/alpdogan/catharsia/entity/User.java
+++ src/main/java/com/alpdogan/catharsia/entity/User.java
@@ -26,12 +26,15 @@ import java.util.Set;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column (name = "id")
     private int id;
 
+    @NotNull
     @Column(name = "first_name")
     private String firstName;
+
+    @NotNull
     @Column(name = "last_name")
     private String lastName;
 
@@ -75,5 +78,12 @@ public class User {
         this.password = password;
     }
 
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 
 }
