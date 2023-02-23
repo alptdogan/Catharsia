f0c80a58a843bb140a66f6be311b0cb16325c90a
Alptuğ Doğan <alpdogan000@gmail.com>
2023-02-23 15:24:18 +0300

    Update

--- src/main/java/com/alpdogan/catharsia/entity/Category.java
+++ src/main/java/com/alpdogan/catharsia/entity/Category.java
@@ -2,36 +2,49 @@ package com.alpdogan.catharsia.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
+//@NoArgsConstructor
 @ToString
 @Entity
-@Table(name = "category")
+@Table(name = "categories")
 public class Category {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column (name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name = "category_name")
-    private String categoryName;
+    @Enumerated(EnumType.STRING)
+    @Column
+    private ECategory name;
 
-    @OneToMany(fetch = FetchType.LAZY, cascade = {
-            CascadeType.PERSIST,
-            CascadeType.MERGE,
-            CascadeType.DETACH,
-            CascadeType.REFRESH
-    })
-    private List<Topic> topics = new ArrayList<>();
+    public Category() {
+
+    }
+
+    public Category(ECategory name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public ECategory getName() {
+        return name;
+    }
+
+    public void setName(ECategory name) {
+        this.name = name;
+    }
 
 
 }
