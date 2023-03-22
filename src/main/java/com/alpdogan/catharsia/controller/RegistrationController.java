3b9f521cb6aa4415ccc850c6a62e31d0cbfdf7be
Alptuğ Doğan <alpdogan000@gmail.com>
2023-03-22 12:51:05 +0300

    Add controllers and templates for register and login operations

---
+++ src/main/java/com/alpdogan/catharsia/controller/RegistrationController.java
@@ -0,0 +1,108 @@
+package com.alpdogan.catharsia.controller;
+
+import com.alpdogan.catharsia.entity.ERole;
+import com.alpdogan.catharsia.entity.User;
+import com.alpdogan.catharsia.repository.RoleRepository;
+import com.alpdogan.catharsia.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/register")
+public class RegistrationController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @GetMapping
+    public String showRegistrationForm(Model model) {
+
+
+        model.addAttribute("user", new User());
+
+        return "signup_form";
+
+    }
+
+    /*
+    @PostMapping
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Username is already taken!"));
+        }
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Email is already in use!"));
+        }
+
+        // Create new user's account
+        User user = new User(signUpRequest.getUsername(),
+                signUpRequest.getEmail(),
+                encoder.encode(signUpRequest.getPassword()));
+
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin":
+                        Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(adminRole);
+
+                        break;
+                    case "mod":
+                        Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(modRole);
+
+                        break;
+                    default:
+                        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(userRole);
+                }
+            });
+        }
+
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+     */
+
+    @PostMapping("/process_register")
+    public String processRegister(User user) {
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        user.setRoles(roleRepository.findByName(ERole.ROLE_USER));
+
+        userRepository.save(user);
+
+        return "register_success";
+
+    }
+
+
+
+}
---
+++ src/main/java/com/alpdogan/catharsia/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.alpdogan.catharsia.repository;
+
+import com.alpdogan.catharsia.entity.ERole;
+import com.alpdogan.catharsia.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Set;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Set<Role> findByName(ERole name);
+
+}
